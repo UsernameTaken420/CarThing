b4bf3d64e123fa5ca6ac6733bde5853c8e8e6beb
Zayfer24 <tabareroland@hotmail.com>
2017-06-08 23:16:36 -0300

    Script
    Script para remover a un usuario de un grupo secundario

---
+++ Scripts/removeuserfromgroup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+d=`date +%S%M%H%d%m%y`
+echo 'Ingrese el nombre del usuario que desea remover del grupo'
+read nomUsuario
+buscar=`cat /etc/passwd | cut -d ':' -f 1 | grep -w $nomUsuario`
+if [ '$nomUsuario' -eq '$buscar' ]
+then
+  echo "Ingrese el nombre del grupo del que quiere removerlo"
+  read nomGrupo
+  buscar=`cat /etc/group | grep $nomGrupo | cut -d ':' -f 4 | grep $nomUsuario -w`
+  if [ -z $buscar ]
+  then
+    echo 'El usuario $nomUsuario no se encuentra en el grupo $nomGrupo (como grupo secundario)'
+  else
+    gpasswd -d $nomUsuario $nomGrupo
+    buscar=`cat /etc/group | grep $nomGrupo | cut -d ':' -f 4 | grep $nomUsuario -w`
+    if [ -z $buscar ]
+    then
+      echo 'El usuario $nomUsuario se elimino correctamente del grupo $nomGrupo'
+      echo 'Se elimino al usuario $nomUsuario del grupo $nomGrupo el $d' >> /root/GestionLogs
+    else
+      echo 'Ocurrio un error al eliminar el usuario del grupo'
+    fi
+  fi
+else
+  echo 'No existe ningun usuario con el nombre que ingreso'
+fi
