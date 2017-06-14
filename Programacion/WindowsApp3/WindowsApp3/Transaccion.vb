fd59ebaee55e3d14bce64e4fe244e95c668b3fe5
Tails99 <nachogolin@hotmail.com>
2017-06-14 11:37:37 -0300

    Add files via upload

---
+++ Programacion/WindowsApp3/WindowsApp3/Transaccion.vb
@@ -0,0 +1,9 @@
+﻿Public Class Transaccion
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs) 
+
+    End Sub
+End Class
